3ab37761020f2a221b44a21ca32ec9331533415c
None <yamldeveloper@proton.me>
2024-02-24 02:35:38 +0300

    change db schema

--- src/ripe_db_search/schema.sql
+++ src/ripe_db_search/schema.sql
@@ -10,15 +10,19 @@ CREATE TABLE IF NOT EXISTS inetnums (
   -- admin_c text,
   -- tech_c text,
   notify text,
-  source text,
-  status character varying(255),
+  source character varying(31),
+  -- alter table inetnums alter column status type character varying(31);
+  status character varying(31),
   created timestamp without time zone,
   last_modified timestamp without time zone
 );
 -- DROP INDEX IF EXISTS inetnums_range_ip_idx;
-CREATE INDEX IF NOT EXISTS inetnums_range_ip_idx ON inetnums(first_ip DESC, last_ip ASC);
-CREATE INDEX IF NOT EXISTS inetnums_source_idx ON inetnums(lower(source));
-CREATE INDEX IF NOT EXISTS inetnums_status_idx ON inetnums(lower(status));
+CREATE INDEX IF NOT EXISTS inetnums_range_ip_idx
+  ON inetnums(first_ip DESC, last_ip ASC);
+CREATE INDEX IF NOT EXISTS inetnums_source_idx
+  ON inetnums(lower(source));
+CREATE INDEX IF NOT EXISTS inetnums_status_idx
+  ON inetnums(lower(status));
 -- Для ускорения используется полнотекстовый поиск
 -- https://github.com/reginalin/postgres-full-text-search
 -- select * from inetnums where search_vector @@ to_tsquery('sber')
@@ -26,10 +30,13 @@ ALTER TABLE inetnums
 ADD COLUMN IF NOT EXISTS search_vector tsvector GENERATED ALWAYS AS (
     to_tsvector(
       'english',
-      COALESCE(netname, '') || ' ' || COALESCE(descr, '') || ' ' || COALESCE(org, '') || ' ' || COALESCE(country, '') || ' ' || COALESCE(mnt_by, '')
+      COALESCE(netname, '') || ' ' ||  COALESCE(descr, '') || ' ' || 
+      COALESCE(org, '') || ' ' || COALESCE(country, '') || ' ' || 
+      COALESCE(mnt_by, '')
     )
   ) STORED;
-CREATE INDEX IF NOT EXISTS inetnums_search_vector_idx ON inetnums USING gin(search_vector);
+CREATE INDEX IF NOT EXISTS inetnums_search_vector_idx
+  ON inetnums USING gin(search_vector);
 -- бесполезные колонки
-ALTER TABLE inetnums DROP COLUMN IF EXISTS admin_c;
-ALTER TABLE inetnums DROP COLUMN IF EXISTS tech_c;
+-- ALTER TABLE inetnums DROP COLUMN IF EXISTS admin_c;
+-- ALTER TABLE inetnums DROP COLUMN IF EXISTS tech_c;
